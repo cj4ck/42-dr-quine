9293bb18e7cdae1ff4541cb911a56350a8d9ce6c
KGebski0036 <kgebski68@gmail.com>
2024-05-23 15:51:21 +0200

    diffrent marco in Grace

--- C/Grace.c
+++ C/Grace.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define PROGRAM "#include <stdio.h>%1$c%1$c#define PROGRAM %2$c%3$s%2$c%1$c%1$c#define END return 0;%1$c%1$c#define MAIN int main() {FILE *file = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); char *s = PROGRAM; fprintf(file, s, 10, 34, s); END}%1$c%1$cMAIN%1$c"
+#define PROGRAM "#include <stdio.h>%1$c%1$c#define PROGRAM %2$c%3$s%2$c%1$c%1$c#define END return 0;%1$c%1$c#define FT(x)int main() {FILE *file = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); char *s = PROGRAM; if(file) fprintf(file, s, 10, 34, s); END}%1$c%1$cFT()%1$c"
 
 #define END return 0;
 
-#define MAIN int main() {FILE *file = fopen("Grace_kid.c", "w"); char *s = PROGRAM; fprintf(file, s, 10, 34, s); END}
+#define FT(x)int main() {FILE *file = fopen("Grace_kid.c", "w"); char *s = PROGRAM; if(file) fprintf(file, s, 10, 34, s); END}
 
-MAIN
+FT()
