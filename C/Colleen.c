9c37445516864d89cea159abd580c0705e6fdca8
KGebski0036 <kgebski68@gmail.com>
2024-05-23 14:00:00 +0200

    Colleen writen i n printf

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+//Hello
+void foo() {
+    return;
+}
 
 int main() {
-    char *program = "#include <stdio.h>%c%cint main() {%c    char *program = %c%s%c;%c    printf(program, 10, 10, 10, 34, program, 34, 10, 10, 10, 10);%c    return 0;%c}%c";
-    printf(program, 10, 10, 10, 34, program, 34, 10, 10, 10, 10);
+    //42
+    foo();
+    char *program = "#include <stdio.h>%c//Hello%cvoid foo() {%c    return;%c}%c%cint main() {%c    //42%c    foo();%c    char *program = %c%s%c;%c    printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, program, 34, 10, 10, 10, 10);%c    return 0;%c}%c";
+    printf(program, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, program, 34, 10, 10, 10, 10);
     return 0;
 }
